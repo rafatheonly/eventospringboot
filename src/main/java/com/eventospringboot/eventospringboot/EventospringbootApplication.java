e11ecd76dc45a8b65500089e297f9f6368cee9c6
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-04-26 16:54:02 -0300

    Reenviando arquivos e diretórios (estrutura).

---
+++ src/main/java/com/eventospringboot/eventospringboot/EventospringbootApplication.java
@@ -0,0 +1,12 @@
+package com.eventospringboot.eventospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventospringbootApplication.class, args);
+	}
+}
