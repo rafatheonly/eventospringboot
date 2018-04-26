c6d01b5224681ecb60af33c0bfa6f9a8cd17b0a7
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-04-26 09:31:05 -0300

    Resetando o repositório.

--- src/main/java/local/demo/repository/EventoRepository.java
+++
@@ -1,8 +0,0 @@
-package local.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import local.demo.models.Evento;
-
-public interface EventoRepository extends CrudRepository<Evento, String> {
-	Evento findByCodigo(long codigo);
-}
